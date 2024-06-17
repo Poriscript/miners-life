f2027f2c46bb65e141405352dc1b3ade327d055a
Poriyon <poriscript.main@gmail.com>
2024-06-17 11:28:59 +0900

    Initial commit

---
+++ src/client/java/green_villager/miners_dream/MinersDreamClient.java
@@ -0,0 +1,10 @@
+package green_villager.miners_dream;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinersDreamClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
